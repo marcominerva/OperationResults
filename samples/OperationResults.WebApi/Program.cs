c2c75187658efded82d88a0b5a2eec218cb43990
Marco Minerva <marco.minerva@gmail.com>
2022-09-09 15:47:20 +0200

    Add initial sample

--- samples/OperationResults.WebApi/Program.cs
+++ samples/OperationResults.WebApi/Program.cs
@@ -1,8 +1,13 @@
+using OperationResults.WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<PeopleService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ samples/OperationResults.WebApi/Services/PeopleService.cs
@@ -0,0 +1,21 @@
+﻿using OperationResults.WebApi.Models;
+
+namespace OperationResults.WebApi.Services;
+
+public class PeopleService
+{
+    public async Task<Result<IEnumerable<Person>>> GetAsync(string queryText)
+    {
+        await Task.Delay(100);
+        var people = new List<Person>();
+
+        return people;
+    }
+
+    public async Task<Result<Person>> GetAsync(Guid id)
+    {
+        await Task.Delay(100);
+
+        return Result.Fail(FailureReasons.ItemNotFound);
+    }
+}
