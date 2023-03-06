274922456a6974e711a680311c4a7da57128e48b
Marco Minerva <marco.minerva@gmail.com>
2023-03-06 16:32:41 +0100

    Add option to directly use HTTP status codes as failure reasons #34

--- samples/MinimalApis/OperationResults.Sample/Program.cs
+++ samples/MinimalApis/OperationResults.Sample/Program.cs
@@ -28,6 +28,10 @@ builder.Services.AddOperationResult(options =>
     // Adding new mappings or editing the existing ones allows to define what HTTP Status Codes the API must return
     // for the Operation Results of our Business Logic methods.
     options.StatusCodesMapping.Add(CustomFailureReasons.NotAvailable, StatusCodes.Status501NotImplemented);
+
+    // If you just want to directly use HTTP status codes as failure reasons, set the following property to true.
+    // In this way, the code you use with Result.Fail() will be used as response status code with no further mapping.
+    //options.UseHttpStatusCodes = true;
 });
 
 builder.Services.AddEndpointsApiExplorer();
