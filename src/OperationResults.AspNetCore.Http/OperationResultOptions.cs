eae25e0ead8741573448b7557856bd9a7fbac9d4
Marco Minerva <marco.minerva@gmail.com>
2024-06-17 10:21:14 +0200

    Refactor HTTP status code handling

--- src/OperationResults.AspNetCore.Http/OperationResultOptions.cs
+++ src/OperationResults.AspNetCore.Http/OperationResultOptions.cs
@@ -4,28 +4,33 @@ namespace OperationResults.AspNetCore.Http;
 
 public class OperationResultOptions
 {
-    public ErrorResponseFormat ErrorResponseFormat { get; set; }
+    public ErrorResponseFormat ErrorResponseFormat { get; set; } = ErrorResponseFormat.Default;
 
     public Dictionary<int, int> StatusCodesMapping { get; }
 
-    public bool UseHttpStatusCodes { get; set; }
+    public bool MapStatusCodes { get; set; } = true;
 
-    public UnmappedFailureReasonBehavior UnmappedFailureReasonBehavior { get; set; }
+    public UnmappedFailureReasonBehavior UnmappedFailureReasonBehavior { get; set; } = UnmappedFailureReasonBehavior.UseDefaultStatusCode;
 
-    public int UnmappedFailureReasonStatusCode { get; set; } = StatusCodes.Status500InternalServerError;
+    public int UnmappedFailureReasonStatusCode { get; set; } = StatusCodes.Status501NotImplemented;
 
     public OperationResultOptions()
     {
         StatusCodesMapping = new Dictionary<int, int>
         {
             [FailureReasons.None] = StatusCodes.Status200OK,
-            [FailureReasons.ItemNotFound] = StatusCodes.Status404NotFound,
-            [FailureReasons.Forbidden] = StatusCodes.Status403Forbidden,
-            [FailureReasons.DatabaseError] = StatusCodes.Status500InternalServerError,
             [FailureReasons.ClientError] = StatusCodes.Status400BadRequest,
-            [FailureReasons.InvalidFile] = StatusCodes.Status415UnsupportedMediaType,
-            [FailureReasons.Conflict] = StatusCodes.Status409Conflict,
             [FailureReasons.Unauthorized] = StatusCodes.Status401Unauthorized,
+            [FailureReasons.Forbidden] = StatusCodes.Status403Forbidden,
+            [FailureReasons.ItemNotFound] = StatusCodes.Status404NotFound,
+            [FailureReasons.InvalidRequest] = StatusCodes.Status406NotAcceptable,
+            [FailureReasons.Timeout] = StatusCodes.Status408RequestTimeout,
+            [FailureReasons.Conflict] = StatusCodes.Status409Conflict,
+            [FailureReasons.InvalidFile] = StatusCodes.Status415UnsupportedMediaType,
+            [FailureReasons.InvalidContent] = StatusCodes.Status422UnprocessableEntity,
+            [FailureReasons.DatabaseError] = StatusCodes.Status500InternalServerError,
+            [FailureReasons.NetworkError] = StatusCodes.Status500InternalServerError,
+            [FailureReasons.ServiceUnavailable] = StatusCodes.Status503ServiceUnavailable,
             [FailureReasons.GenericError] = StatusCodes.Status500InternalServerError
         };
     }
